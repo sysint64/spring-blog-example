24c26c57ee5447af0b3f57182de583de3328bf82
Andrey Kabylin <andrey@kabylin.ru>
2018-10-15 16:59:09 +0700

    initial commit

---
+++ src/main/kotlin/im/dialog/sharechatbotapi/Repositories.kt
@@ -0,0 +1,9 @@
+package im.dialog.sharechatbotapi
+
+import org.springframework.data.repository.CrudRepository
+
+interface ArticleRepository : CrudRepository<Article, Long> {
+    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+}
+
+interface UserRepository : CrudRepository<User, String>
---
+++ src/main/kotlin/im/dialog/sharechatbotapi/SharechatbotapiApplication.kt
@@ -0,0 +1,11 @@
+package im.dialog.sharechatbotapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharechatbotapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SharechatbotapiApplication>(*args)
+}
